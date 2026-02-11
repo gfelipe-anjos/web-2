797f716289197ecc814c22edb90166a981526fa5
gfelipe-anjos <135139283+gfelipe-anjos@users.noreply.github.com>
2026-02-11 11:37:37 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let vidas = 3;
+let naves = 0;
+let segundos = 00;
+let minutos = 00;
+let horas = 00;
+let lateral=0;
+let altura =0;
+
+function add(){
+    segundos++
+    document.querySelector("#segundos").textContent = segundos
+}
+
