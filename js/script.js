ad3aa452b11680798fcd3af5b1221a16b80e4870
gfelipe-anjos <135139283+gfelipe-anjos@users.noreply.github.com>
2026-03-04 09:58:16 -0300

    finalmente movimentação funcionando

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let vidas = 3;
 let naves = 0;
-let segundos = 00;
-let minutos = 00;
-let horas = 00;
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
 let lateral=0;
 let altura =0;
 
-function add(){
+function tempo(){
     segundos++
     document.querySelector("#segundos").textContent = segundos
 }
